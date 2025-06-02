7d29bba406f13cd7ab73cc9749ec432a59808650
Hervé Guétin <herve.guetin@dnd.fr>
2025-06-02 23:20:08 +0200

    wip

---
+++ web/routes/wishlist.$id.ts
@@ -0,0 +1,7 @@
+import { CustomFunctionArgs } from "types";
+import { getCustomerWishlists, makeAuthContext } from "../../modules";
+
+export async function loader({ request, context }: CustomFunctionArgs) {
+  await makeAuthContext({ request, context });
+  return await getCustomerWishlists(context, context.customerGid!);
+}
